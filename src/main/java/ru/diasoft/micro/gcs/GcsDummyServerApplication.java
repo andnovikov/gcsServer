9363c3b4bfae135e8f0fe24ff8fc29332e44ac58
andnovikov <andnovikov@outlook.com>
2020-07-01 19:03:00 +0300

    Initial commit.

---
+++ src/main/java/ru/diasoft/micro/gcs/GcsDummyServerApplication.java
@@ -0,0 +1,12 @@
+package ru.diasoft.micro.gcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcsDummyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcsDummyServerApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/diasoft/micro/gcs/GcsDummyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.diasoft.micro.gcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcsDummyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
